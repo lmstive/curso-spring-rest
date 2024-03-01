d1eda68ee94c3e65f5eb46289b52222b45818d24
lmstive <lmstive@gmail.com>
2024-03-01 17:27:50 -0300

    2.14 Injeçao e anotaçao @Autowired

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -4,28 +4,14 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-
-	private Boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail ");
-	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if (this.caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
-		System.out.printf("Notificando %s através do e-mail %s: usando SMTP %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
-	}
 
-	public void setCaixaAlta(Boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(),
+				mensagem);
 	}
 
 }
