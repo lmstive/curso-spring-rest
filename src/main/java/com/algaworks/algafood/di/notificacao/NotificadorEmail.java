9b27cabe3544bb7466539f59993ba81e77917687
lmstive <lmstive@gmail.com>
2024-03-01 10:43:01 -0300

    2.12 Injetando dependencias Beans

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.di.modelo.Cliente;
 
 @Component
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
 
-	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), mensagem);
+	public NotificadorEmail() {
+		System.out.println("NotificadorEmail ");
 	}
-	
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+
 }
